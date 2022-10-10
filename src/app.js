cf59c98fdb98e671f36831e10c285c5389b43ec6
beaesreal <beaesreal@gmail.com>
2022-10-10 12:41:24 +0000

    Random Card Generator done

--- src/app.js
+++ src/app.js
@@ -7,5 +7,69 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  document.querySelector(".card").classList.add(generateRandomSuit());
+  document.querySelector(".number").innerHTML = generateRandomNumber();
 };
+
+window.onclick = function() {
+  document
+    .querySelector("#generateRandomCard")
+    .addEventListener("click", () => {
+      document.querySelector(".card").classList.add(generateRandomSuit());
+      document.querySelector(".number").innerHTML = generateRandomNumber();
+    });
+
+  document.querySelector("#timer").addEventListener("click", () => {
+    setInterval(() => {
+      document.querySelector(".card").classList.add(generateRandomSuit());
+      document.querySelector(".number").innerHTML = generateRandomNumber();
+    }, 1000);
+    false;
+  });
+};
+
+let generateRandomNumber = () => {
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let indexNumbers = Math.floor(Math.random() * numbers.length);
+
+  return numbers[indexNumbers];
+};
+
+let generateRandomSuit = () => {
+  let suit = ["diamond", "spade", "heart", "club"];
+  let indexSuit = Math.floor(Math.random() * 4);
+
+  return suit[indexSuit];
+};
+
+/*var btn = document.getElementById("generateRandomCard");
+btn.addEventListener("click", generateRandomNumber);
+
+/*var btn = document.querySelector("#generateRandomCard");
+btn.addEventListener("click", generateRandomNumber);
+btn.addEventListener("click", generateRandomSuit);
+
+setTimeout(() => {
+  document.querySelector(".card").classList.add(generateRandomSuit());
+  document.querySelector(".number").innerHTML = generateRandomNumber();
+}, 1000); // 1 second
+
+let btn = document.getElementById("generateRandomCard");
+btn.addEventListener("click", generateRandomSuit);
+btn.addEventListener("click", generateRandomNumber);
+*/
